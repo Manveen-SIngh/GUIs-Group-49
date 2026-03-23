a1d5d81f61dd409f32615f06089c1bb3d975d0ed
Manveen-SIngh <ec24899@qmul.ac.uk>
2026-03-23 20:00:57 +0000

    started Outdoor activities page, added rubik font to family and menu icon

--- weather-app/src/OdA.js
+++ weather-app/src/OdA.js
@@ -1,12 +1,6 @@
 import bg from "./assets/PartlyCloudy.png";
+import menuIcon from "./assets/menu.svg";
 
-import TopBarMetricPage from "./components/TopBarMetricPage";
-import WeatherBox from "./components/WeatherBox";
-import WindBox from "./components/WindBox";
-import SunriseSunsetBox from "./components/SunriseSunsetBox";
-import UVBox from "./components/UVBox";
-import VisibilityBox from "./components/VisibilityBox";
-import HumidityBox from "./components/HumidityBox";
 
 function OdA() {
   return (
@@ -18,13 +12,48 @@ function OdA() {
         backgroundPosition: "center",
         display: "flex",
         justifyContent: "center",
-        alignItems: "flex-start",
-        padding: "40px 0", 
-        boxSizing: "border-box",
+        
+        
       }}
     >
-      
+    <div style={{width: '100%', height: '100%', position: 'absolute', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}}>
+      <div style={{width: 165, height: 91, left: 1700, top: 42, position: 'absolute', background: 'rgba(255, 255, 255, 0.50)', borderRadius: 28}} />
+        <div style={{ position: "absolute",left: 1700,top: 42,width: 165,height: 91,borderRadius: 24,display: "flex",alignItems: "center",justifyContent: "center",font: "Rubik",fontSize: 50,fontWeight: 500,color: "black",}}>12:11</div>
     </div>
+
+    <div style={{width: '100%', height: '100%', position: 'absolute'}}>
+      <div style={{width: 139, height: 53, left: 351, top: 55, position: 'absolute', background: 'rgba(255, 255, 255, 0.50)', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 43}} />
+      <div style={{width: 79.11, height: 53, left: 351, top: 55, position: 'absolute', background: 'rgba(203, 210, 208, 0.40)', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 43}} />
+      <div style={{width: 64.41, left: 367, top: 64, position: 'absolute', color: 'black', fontSize: 32, fontFamily: 'Rubik', fontWeight: '700', wordWrap: 'break-word'}}>mi</div>
+      <div style={{width: 48.59, left: 435, top: 64, position: 'absolute', color: 'black', fontSize: 32, fontFamily: 'Rubik', fontWeight: '700', wordWrap: 'break-word'}}>km</div>
+    </div>
+
+    <div style={{width: '100%', height: '100%', position: 'absolute'}}>
+      <div style={{width: 123, height: 53, left: 178, top: 55, position: 'absolute', background: 'rgba(255, 255, 255, 0.50)', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 43}} />
+      <div style={{width: 70, height: 53, left: 178, top: 55, position: 'absolute', background: 'rgba(203, 210, 208, 0.40)', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 43}} />
+      <div style={{width: 43, left: 186, top: 64, position: 'absolute', color: 'black', fontSize: 32, fontFamily: 'Rubik', fontWeight: '700', wordWrap: 'break-word'}}>°C</div>
+      <div style={{width: 43, left: 258, top: 64, position: 'absolute', color: 'black', fontSize: 32, fontFamily: 'Rubik', fontWeight: '700', wordWrap: 'break-word'}}>°F</div>
+    </div>
+
+    <div style={{width: '100%', height: '100%', position: 'absolute'}}>
+      <div style={{width: 91, height: 91, left: 37, top: 36, position: 'absolute', background: 'rgba(255, 255, 255, 0.50)', borderRadius: 28}} />
+      <div style={{width: 60, height: 60, left: 37, top: 36, position: 'absolute'}}>
+          <div style={{width: '100%', height: '100%', position: 'absolute',left: 15 , top: 15.5}}><img src={menuIcon}/>
+        </div>
+      </div>
+    </div>
+
+    <div style={{width: '100%', height: '100%', position: 'absolute'}}>
+      <div style={{ position: "absolute",left: 40,top: 130,alignItems: "left",font: "Rubik",fontSize: 52,fontWeight: 400,color: "black"}}>Outdoor Activity Summary</div>
+    </div>
+
+    <div style={{width: '100%', height: '100%', position: 'absolute'}}>
+      <div style={{ position: "absolute",left: 40,top: 173,alignItems: "left",font: "Rubik",fontSize: 92,fontWeight: 500,color: "black"}}>Cycling</div>
+    </div>
+    
+
+  </div>
+    
   );
 }
 
